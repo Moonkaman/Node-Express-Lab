50250e4d486def82d94170a8339a6d349cddb0fa
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-12 14:04:39 -0700

    Create post endpoint in postsRouter

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -4,6 +4,16 @@ const db = require('../data/db');
 
 const router = express.Router();
 
-router.get('/', (req, res) => db.find().then(posts => res.status(200).json(posts)).catch(err => res.status(500).json({error: 'The posts information could not be retrieved.'})));
+router.get('/', (req, res) => db.find().then(posts => res.status(200).json(posts)).catch(err => res.status(500).json({errorMessage: 'The posts information could not be retrieved.', error: err})));
+
+router.post('/', (req, res) => {
+  if(!req.body.title || !req.body.contents){
+    res.status(400).json({errorMessage: 'Please provide title and contents for the post.'})
+  } else {
+    db.insert(req.body)
+      .then(id => db.findById(id.id).then(post => res.status(201).json(post)).catch(err => res.status(500).json({errorMessage: 'The posts information could not be retrieved.', error: err})))
+      .catch(err => res.status(500).json({errorMessage: 'The posts information could not be retrieved.', error: err}));
+  }
+})
 
 module.exports = router;
